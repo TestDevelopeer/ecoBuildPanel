1251f955d88907dd00eda446399dd5bb2f2ef398
TestDevelopeer <testdeveloper7967@gmail.com>
2022-10-12 18:22:52 +0300

    Стартовая настройка окружения

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+<!-- BEGIN: Head-->
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Register Multi Steps Page - Vuexy - Bootstrap HTML admin template</title>
+    <link rel="apple-touch-icon" href="/app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="/app-assets/images/ico/favicon.ico">
+    <link href="/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
+
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="/app-assets/vendors/css/vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="/app-assets/vendors/css/forms/wizard/bs-stepper.min.css">
+    <link rel="stylesheet" type="text/css" href="/app-assets/vendors/css/forms/select/select2.min.css">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="/app-assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/app-assets/css/bootstrap-extended.min.css">
+    <link rel="stylesheet" type="text/css" href="/app-assets/css/colors.min.css">
+    <link rel="stylesheet" type="text/css" href="/app-assets/css/components.min.css">
+    <link rel="stylesheet" type="text/css" href="/app-assets/css/themes/dark-layout.min.css">
+    <link rel="stylesheet" type="text/css" href="/app-assets/css/themes/bordered-layout.min.css">
+    <link rel="stylesheet" type="text/css" href="/app-assets/css/themes/semi-dark-layout.min.css">
+
+    @yield('otherStyles')
+
+    <!-- BEGIN: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+    <!-- END: Custom CSS-->
+</head>
+<!-- END: Head-->
+
+<body class="horizontal-layout horizontal-menu content-left-sidebar navbar-floating footer-static" data-open="hover" data-menu="horizontal-menu" data-col="content-left-sidebar">
+    @include('layouts.header')
+    @include('layouts.menu')
+    @yield('content')
+    @include('layouts.footer')
+    <!-- BEGIN: Vendor JS-->
+    <script src="/app-assets/vendors/js/vendors.min.js"></script>
+    <!-- BEGIN Vendor JS-->
+
+    <!-- BEGIN: Page Vendor JS-->
+    <script src="/app-assets/vendors/js/ui/jquery.sticky.js"></script>
+    <script src="/app-assets/vendors/js/editors/quill/katex.min.js"></script>
+    <script src="/app-assets/vendors/js/editors/quill/highlight.min.js"></script>
+    <script src="/app-assets/vendors/js/editors/quill/quill.min.js"></script>
+    <script src="/app-assets/vendors/js/extensions/toastr.min.js"></script>
+    <script src="/app-assets/vendors/js/forms/select/select2.full.min.js"></script>
+    <!-- END: Page Vendor JS-->
+
+    <!-- BEGIN: Theme JS-->
+    <script src="/app-assets/js/core/app-menu.min.js"></script>
+    <script src="/app-assets/js/core/app.min.js"></script>
+    <script src="/app-assets/js/scripts/customizer.min.js"></script>
+    <!-- END: Theme JS-->
+
+    @yield('otherScripts')
+
+    <script>
+        $(window).on('load',  function(){
+            if (feather) {
+            feather.replace({ width: 14, height: 14 });
+            }
+        })
+    </script>
+</body>
+
+</html>
